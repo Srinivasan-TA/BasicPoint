3d6793cafc1cec0035581ee36864d3ce4f5348b1
srini <srinivasan.k@tringapps.com>
2023-03-01 16:57:13 +0530

    commit 2nd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
-
 class Main {
     public static void main(String[] args) throws CloneNotSupportedException{
         Scanner se = new Scanner(System.in);
@@ -15,9 +14,9 @@ class Main {
         e.info(r);
         int y = se.nextInt();
         Point p1 = new Point(x,y);
-        System.out.println(p1.x+ p1.y);
+        e.info(String.valueOf(p1.x+ p1.y));
         Point p2 = p1.clone();
-        System.out.println(p2.x+ p2.y);
+        e.info(String.valueOf(p2.x+ p2.y));
         String a =String.valueOf(p1.equalling(p2));
         e.info(a);
         p2.display();
